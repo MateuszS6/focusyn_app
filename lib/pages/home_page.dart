4edaa7d5d63d45bd68d8cdc52df7df2e088a4df6
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-16 17:39:47 +0000

    Plan initial home page layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,18 +25,53 @@ class _HomePageState extends State<HomePage> {
             ),
             const SizedBox(height: 16.0),
             Text(
-              'Motivational quote',
+              'Something here',
               style: TextStyle(
                 fontSize: 16.0,
                 fontStyle: FontStyle.italic,
               ),
             ),
-            const SizedBox(height: 64.0),
+            const SizedBox(height: 48.0),
             Text(
-              'Your Focuses',
+              'Today\'s Breakdown',
               style: TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'You have 3 tasks due today.\nYou have 1 task overdue.',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const SizedBox(height: 48.0),
+            Text(
+              'Feeling Overwhelmed?',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '[Button]\nTry to break down your tasks into smaller, more manageable chunks.',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(height: 48.0),
+            Text(
+              'Quick Links',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '[Button] Self-care\n[Button] Motivational quote',
+              style: TextStyle(
+                fontSize: 16.0,
               ),
             ),
           ],
