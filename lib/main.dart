3318b4c847ff5750779be4f81b5208232f0cd01f
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-16 15:58:28 +0000

    Create main screen nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Focusyn [Alpha]',
       debugShowCheckedModeBanner: false,
+      home: MainScreen(),
     );
   }
 }
---
+++ lib/main_screen.dart
@@ -0,0 +1,51 @@
+import 'package:focusyn_app/pages/planner_page.dart';
+import 'package:focusyn_app/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = <Widget>[
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
