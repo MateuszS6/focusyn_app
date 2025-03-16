026adf8afa64719969addaa36e025343cf38185f
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-16 16:05:56 +0000

    Add padding to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,5 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+      ),
+    );
   }
 }
