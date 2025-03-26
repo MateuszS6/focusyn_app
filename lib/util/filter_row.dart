1dbb59ca6165c7487e7bc5af362391b9fa3aa310
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-26 23:28:58 +0000

    Add filters and tag management

---
+++ lib/util/filter_row.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class FilterRow extends StatelessWidget {
+  final List<String> filters;
+  final Set<String> hidden;
+  final String selected;
+  final void Function(String) onSelect;
+  final VoidCallback onAdd;
+
+  const FilterRow({
+    super.key,
+    required this.filters,
+    required this.hidden,
+    required this.selected,
+    required this.onSelect,
+    required this.onAdd,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final visibleFilters = filters.where((f) => !hidden.contains(f)).toList();
+
+    return SizedBox(
+      height: 40,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: visibleFilters.length + 1,
+        separatorBuilder: (_, __) => SizedBox(width: 8),
+        itemBuilder: (context, index) {
+          if (index < visibleFilters.length) {
+            final tag = visibleFilters[index];
+            final isSelected = tag == selected;
+            return ChoiceChip(
+              label: Text(tag),
+              shape: StadiumBorder(),
+              selected: isSelected,
+              onSelected: (_) => onSelect(tag),
+              selectedColor: Colors.blue,
+              backgroundColor: Colors.grey[200],
+              labelStyle: TextStyle(color: isSelected ? Colors.white : Colors.black),
+            );
+          } else {
+            return ActionChip(
+              label: Icon(Icons.add, size: 18),
+              shape: StadiumBorder(),
+              onPressed: onAdd,
+              backgroundColor: Colors.grey[200],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
