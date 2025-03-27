c56b2903966cea49cacc2d5bdbcf2b60835f64af
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-27 23:52:06 +0000

    Create moment_tile.dart

---
+++ lib/task_tiles/moment_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'base_task_tile.dart';
+
+class MomentTile extends BaseTaskTile {
+  const MomentTile({
+    super.key,
+    required super.task,
+    required super.onEdit,
+  });
+
+  @override
+  State<MomentTile> createState() => _MomentTileState();
+}
+
+class _MomentTileState extends BaseTaskTileState<MomentTile> {
+  @override
+  String getInitialText() => widget.task["title"] ?? '';
+
+  @override
+  Widget buildSubtitle() {
+    final loc = widget.task["location"];
+    final locText = loc != null && loc != "" ? ' • Location: $loc' : '';
+    return Text(
+      "Date: ${widget.task["date"] ?? 'N/A'} • Time: ${widget.task["time"] ?? 'N/A'}$locText",
+      style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+    );
+  }
+}
