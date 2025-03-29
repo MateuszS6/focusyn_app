7505378e9f9448ad64002fac87492eb3c2aa9a9f
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-03-29 14:19:00 +0000

    Rename profile page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +11,7 @@ class ProfilePage extends StatelessWidget {
           icon: Icon(Icons.arrow_back_ios_rounded),
           onPressed: () => Navigator.pop(context),
         ),
-        title: Text("Profile"),
+        title: Text("Account"),
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
