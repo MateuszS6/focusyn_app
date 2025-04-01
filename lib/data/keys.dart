c99437ce82aadb48a9cee7c34f24121baeedc3f4
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-01 23:21:33 +0100

    Create keys.dart

---
+++ lib/data/keys.dart
@@ -0,0 +1,26 @@
+abstract class Keys {
+  static const String homeBox = 'homeBox';
+  static const String taskBox = 'taskBox';
+  static const String filterBox = 'filterBox';
+
+  static const String actions = 'Actions';
+  static const String flows = 'Flows';
+  static const String moments = 'Moments';
+  static const String thoughts = 'Thoughts';
+
+  static const String title = 'title';
+  static const String text = 'text';
+  static const String priority = 'priority';
+  static const String brainPoints = 'brainPoints';
+  static const String date = 'date';
+  static const String time = 'time';
+  static const String duration = 'duration';
+  static const String location = 'location';
+  static const String repeat = 'repeat';
+  static const String history = 'history';
+  static const String createdAt = 'createdAt';
+
+  static const String tag = 'tag';
+  static const String hidden = 'hidden';
+  static const String all = 'All';
+}
