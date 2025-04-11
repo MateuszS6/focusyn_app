c98a544c41b1bedfbf2dd5738d1f6de9abba27ae
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-11 14:50:41 +0100

    Update main_screen.dart
    Optimisation

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:focusyn_app/data/keys.dart';
-
-import 'pages/focuses_page.dart';
-import 'pages/home_page.dart';
-import 'pages/planner_page.dart';
+import 'package:focusyn_app/pages/focuses_page.dart';
+import 'package:focusyn_app/pages/home_page.dart';
+import 'package:focusyn_app/pages/planner_page.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,38 +12,32 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final Set<StatefulWidget> _pages = {HomePage(), FocusesPage(), PlannerPage()};
-
   int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [HomePage(), FocusesPage(), PlannerPage()];
+
+  static const List<BottomNavigationBarItem> _navItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: Keys.home),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.view_agenda_rounded),
+      label: Keys.focuses,
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.local_library_rounded),
+      label: Keys.planner,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: _pages.elementAt(_selectedIndex),
-      bottomNavigationBar: _buildNavBar(),
-    );
-  }
-
-  /// Builds the navigation bar widget.
-  BottomNavigationBar _buildNavBar() {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_rounded),
-          label: Keys.home,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.view_agenda_rounded),
-          label: Keys.focuses,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.local_library_rounded),
-          label: Keys.planner,
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: _navItems,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
