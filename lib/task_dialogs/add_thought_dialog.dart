e71a61783c7d0e5cffa4aae6fe29c9f99a63c00f
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-14 16:17:43 +0100

    Update add_thought_dialog.dart

--- lib/task_dialogs/add_thought_dialog.dart
+++ lib/task_dialogs/add_thought_dialog.dart
@@ -4,7 +4,7 @@ import 'package:focusyn_app/data/keys.dart';
 import 'package:focusyn_app/models/task_model.dart';
 import 'package:focusyn_app/util/task_dialog.dart';
 
-class AddThoughtDialog extends StatelessWidget {
+class AddThoughtDialog extends StatefulWidget {
   static const String _dialogTitle = "Add Thought";
   static const String _textLabel = "Text";
   static const String _tagLabel = "Tag";
@@ -14,30 +14,62 @@ class AddThoughtDialog extends StatelessWidget {
   const AddThoughtDialog({super.key, required this.onAdd});
 
   @override
+  State<AddThoughtDialog> createState() => _AddThoughtDialogState();
+}
+
+class _AddThoughtDialogState extends State<AddThoughtDialog> {
+  String text = '';
+  String tag = Keys.all;
+  late final List<String> tags;
+
+  @override
+  void initState() {
+    super.initState();
+    tags = AppData.instance.filters[Keys.thoughts] ?? [Keys.all];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String text = '';
-    String tag = Keys.all;
-    final tags = AppData.instance.filters[Keys.thoughts] ?? [Keys.all];
+    final inputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: Colors.grey.shade300),
+    );
+
+    final inputDecoration = InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      border: inputBorder,
+      enabledBorder: inputBorder,
+      focusedBorder: inputBorder,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+    );
 
     return TaskDialog(
-      title: _dialogTitle,
-      onAdd: onAdd,
+      title: AddThoughtDialog._dialogTitle,
+      onAdd: widget.onAdd,
       validateInput: () => text.trim().isNotEmpty,
       buildTask: () => Task(title: text, tag: tag, text: text),
       fields: [
         TextField(
-          decoration: const InputDecoration(labelText: _textLabel),
+          decoration: inputDecoration.copyWith(
+            labelText: AddThoughtDialog._textLabel,
+            prefixIcon: const Icon(Icons.lightbulb_rounded),
+            alignLabelWithHint: true,
+          ),
           maxLines: 5,
-          onChanged: (val) => text = val,
+          onChanged: (val) => setState(() => text = val),
         ),
         DropdownButtonFormField<String>(
           value: tag,
-          decoration: const InputDecoration(labelText: _tagLabel),
+          decoration: inputDecoration.copyWith(
+            labelText: AddThoughtDialog._tagLabel,
+            prefixIcon: const Icon(Icons.label_rounded),
+          ),
           items:
               tags
                   .map((t) => DropdownMenuItem(value: t, child: Text(t)))
                   .toList(),
-          onChanged: (val) => tag = val ?? Keys.all,
+          onChanged: (val) => setState(() => tag = val ?? Keys.all),
         ),
       ],
     );
