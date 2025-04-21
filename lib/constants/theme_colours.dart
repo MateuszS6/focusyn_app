53476668aa721517d8a60ce2c716390a2d6aafd2
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-22 00:39:21 +0100

    Update theme_colours.dart
    Comments

--- lib/constants/theme_colours.dart
+++ lib/constants/theme_colours.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// A collection of color constants used throughout the app for consistent theming.
+/// This class defines the color palette for the entire application, including
+/// UI elements, focus categories, and task-related colors.
 class ThemeColours {
-  // UI
+  // UI Colors
   static const Color scaffoldBackgroundColor = Colors.white;
   static const Color appBarBackgroundColor = Colors.white;
   static const Color appBarTextColor = Colors.black;
   static const Color selectedItemColor = Colors.blue;
   static const Color unselectedItemColor = Colors.grey;
 
-  // Focuses
+  // Focuses Category Colors
   static const Color actionsMain = Colors.purple;
   static const Color actionsAlt = Color(0xFFF3E5F5);
   static const Color flowsMain = Colors.lightGreen;
@@ -18,7 +21,7 @@ class ThemeColours {
   static const Color thoughtsMain = Colors.orange;
   static const Color thoughtsAlt = Color(0xFFFFF3E0);
 
-  // Tasks
+  // Task-related Colors
   static const Color taskMain = Color(0xFF64B5F6);
   static const Color taskAlt = Color(0xFFE3F2FD);
 }
