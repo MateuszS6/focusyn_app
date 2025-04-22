9eb10f10b631dc2a43296d8d87d142443b5ef64a
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-22 01:32:00 +0100

    Update thought_tile.dart
    Comments

--- lib/task_tiles/thought_tile.dart
+++ lib/task_tiles/thought_tile.dart
@@ -4,12 +4,30 @@ import 'package:focusyn_app/constants/theme_colours.dart';
 import 'package:focusyn_app/utils/task_tile.dart';
 import 'package:focusyn_app/models/task_model.dart';
 
+/// A specialized task tile widget for displaying and managing thought tasks.
+/// This widget provides:
+/// - Visual representation of a thought task
+/// - Simple title and list information
+/// - Edit and delete functionality
 class ThoughtTile extends StatelessWidget {
+  /// The thought task to display
   final Task task;
+
+  /// Callback function when the task is edited
   final VoidCallback onEdit;
+
+  /// Callback function when the task is deleted
   final VoidCallback onDelete;
+
+  /// Currently selected list name
   final String selectedList;
 
+  /// Creates a thought task tile with the specified properties.
+  ///
+  /// [task] - The thought task to display
+  /// [onEdit] - Callback when the task is edited
+  /// [onDelete] - Callback when the task is deleted
+  /// [selectedList] - Currently selected list name
   const ThoughtTile({
     super.key,
     required this.task,
