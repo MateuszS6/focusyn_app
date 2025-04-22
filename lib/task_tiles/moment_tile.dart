170e2755ee632b8bad8d632dcf272a10ca766e59
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2025-04-22 01:31:56 +0100

    Update moment_tile.dart
    Comments

--- lib/task_tiles/moment_tile.dart
+++ lib/task_tiles/moment_tile.dart
@@ -4,12 +4,31 @@ import 'package:focusyn_app/constants/keys.dart';
 import 'package:focusyn_app/utils/task_tile.dart';
 import 'package:focusyn_app/models/task_model.dart';
 
+/// A specialized task tile widget for displaying and managing moment tasks.
+/// This widget provides:
+/// - Visual representation of a moment task
+/// - Location and duration information
+/// - Overdue task highlighting
+/// - Edit and delete functionality
 class MomentTile extends StatelessWidget {
+  /// The moment task to display
   final Task task;
+
+  /// Callback function when the task is edited
   final VoidCallback onEdit;
+
+  /// Callback function when the task is deleted
   final VoidCallback onDelete;
+
+  /// Currently selected list name
   final String selectedList;
 
+  /// Creates a moment task tile with the specified properties.
+  ///
+  /// [task] - The moment task to display
+  /// [onEdit] - Callback when the task is edited
+  /// [onDelete] - Callback when the task is deleted
+  /// [selectedList] - Currently selected list name
   const MomentTile({
     super.key,
     required this.task,
